7382f0e6d2a6f0131e7df49c3dda615656a94267
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-14 01:13:26 +0600

    Not google user login, signout user

--- src/App.js
+++ src/App.js
@@ -24,14 +24,38 @@ function App() {
       }
       setUser(signedInUser);
     })
+    .catch(error => {
+      alert('Something went wrong!!')
+    })
   }
+
+  //Sign out
+  const handleSignOut = () => {
+    console.log('clicked');
+    firebase.auth().signOut()
+    .then(res => {
+      const signOutUser = {
+        isSignedIn: false,
+        name: '',
+        email: ''
+      }
+      setUser(signOutUser)
+    })
+    .catch(error => {})
+  }
+
   return (
     <div className="App">
+      
       {
         user.isSignedIn && <div><p>Welcome, {user.name}</p>
         <p>Your email: {user.email}</p></div>
       }
-      <button onClick={handleSignIn}>Sign in</button>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>Sign out</button> :
+        <button onClick={handleSignIn}>Sign in</button>
+      }
+      {/* <button onClick={handleSignIn}>Sign in</button> */}
     </div>
   );
 }
