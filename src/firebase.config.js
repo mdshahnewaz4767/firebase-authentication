6fd9233e87fa772be1ec61a1a31e29ea7784e887
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-14 00:30:19 +0600

    Sign in with google, open google login popup

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBgmB-Jx-ODPAN8NEC6wf0_fe5BvR2eVYw",
+  authDomain: "ema-john-simple-cc053.firebaseapp.com",
+  projectId: "ema-john-simple-cc053",
+  storageBucket: "ema-john-simple-cc053.appspot.com",
+  messagingSenderId: "508916971457",
+  appId: "1:508916971457:web:8ec45e988365ce0661a140",
+  measurementId: "G-X2MTF421S6"
+};
+
+export default firebaseConfig;
