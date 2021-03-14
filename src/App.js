e5f0cac707dffbff024b51ee2d588e0072381df6
shahnewaz601 <shahnewaz601@gmail.com>
2021-03-14 22:25:45 +0600

    How to handle facebook login

--- src/App.js
+++ src/App.js
@@ -18,9 +18,10 @@ function App() {
     success: false
   });
 
-  const provider = new firebase.auth.GoogleAuthProvider();
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
   const handleSignIn = () => {
-    firebase.auth().signInWithPopup(provider)
+    firebase.auth().signInWithPopup(googleProvider)
     .then(res => {
       const {displayName, email} = res.user;
       const signedInUser = {
@@ -31,8 +32,32 @@ function App() {
       setUser(signedInUser);
     })
     .catch(error => {
-      alert('Something went wrong!!')
+      console.log(error);
+      console.log(error.message);
     })
+  }
+
+  //Fb Sign in
+  const handleFbSignIn = () => {
+    firebase.auth().signInWithPopup(fbProvider)
+    .then((result) => {
+      var credential = result.credential;
+      // The signed-in user info.
+      var user = result.user;
+      console.log('fb user sign in', user);
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var accessToken = credential.accessToken;
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+
+      // ...
+    });
   }
 
   //Sign out
@@ -122,16 +147,19 @@ function App() {
 
   return (
     <div className="App">
-      
-      {
-        user.isSignedIn && <div><p>Welcome, {user.name}</p>
-        <p>Your email: {user.email}</p></div>
-      }
+
+
       {
         user.isSignedIn ? <button onClick={handleSignOut}>Sign out</button> :
         <button onClick={handleSignIn}>Sign in</button>
       }
+      <br/>
       {/* <button onClick={handleSignIn}>Sign in</button> */}
+      <button onClick={handleFbSignIn}>Sign in using Facebook</button>
+      {
+        user.isSignedIn && <div><p>Welcome, {user.name}</p>
+        <p>Your email: {user.email}</p></div>
+      }
 
       <h1>Our own Authentication</h1>
       <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="newUser"/>
